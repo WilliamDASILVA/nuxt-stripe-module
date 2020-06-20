27ca039f38c5ce6efe18f730c735ed668555de58
William DA SILVA <william.da.silva@outlook.com>
2020-06-20 23:51:57 +0200

    chore: update dev dependencies (#9)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/lib/$1',
+    '^~~$': '<rootDir>',
+    '^@@$': '<rootDir>',
+    '^@/(.*)$': '<rootDir>/lib/$1'
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
+}
